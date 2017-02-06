4aa43ea483b0e4f071ee6273f155e6fce711ca25
kai <328458211@qq.com>
2017-02-06 16:21:50 +0800

    because of bad job in admin authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+#  before_action :admin_required
+# (暂时换用job-listing中的办法)
+
+#  def admin_required
+#    if !current_user.admin?
+#      redirect_to "/"
+#    end
+#  end
 end
